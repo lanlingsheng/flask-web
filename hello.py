59557dc3d93c586f1b3318b29472dde5bbb9fd93
lanlingsheng <gaoyan987@gmail.com>
2016-10-18 14:14:58 +0800

    add email

--- hello.py
+++ hello.py
@@ -4,11 +4,12 @@ from flask import render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,6 +17,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY']= 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
+
+# mail server setting
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'gaoyan0116@hotmail.com'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 manager = Manager(app)
@@ -24,6 +36,14 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+ ' '+ subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    msg.send(msg)
 
 def make_shell_context():
     return dict(app = app, db = db, User= User, Role = Role)
@@ -47,7 +67,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is yout name?', validators=[Required()])
     submit = SubmitField('Submit')
 
@@ -60,30 +80,20 @@ def index():
             user = User(username = form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user = user)
         else:
             session['known'] = True
         session['name'] = form.name.data
-        form.name.data = ''
+        form.nam.data = ''
         return redirect(url_for('index'))
     return render_template('index.html', form = form, name = session.get('name'),known = session.get('known', False) )
 
-'''
-def index():
-    return render_template('index.html', current_time= datetime.utcnow())
-'''
-'''
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name = name)
-'''
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     db.create_all()
